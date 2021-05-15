e0c1ad4bbe470398c6641648a1f24ab781616e40
Ash Williams <ashthewill@gmail.com>
2021-05-15 19:34:47 -0400

    day 4 exercises

--- module-1/04_Loops_Arrays/exercise-student/java/src/main/java/com/techelevator/Exercises.java
+++ module-1/04_Loops_Arrays/exercise-student/java/src/main/java/com/techelevator/Exercises.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import javax.swing.*;
+
 public class Exercises {
 	
 	/*
@@ -10,6 +12,10 @@ public class Exercises {
 	 firstLast6([13, 6, 1, 2, 3]) → false
 	 */
 	public boolean firstLast6(int[] nums) {
+		if(nums[0] == 6 || nums[nums.length-1] == 6)
+			return true;
+
+		else
 		return false;
 	}
 
@@ -21,6 +27,10 @@ public class Exercises {
 	 sameFirstLast([1, 2, 1]) → true
 	 */
 	public boolean sameFirstLast(int[] nums) {
+		if(nums[0] == nums[nums.length-1])
+			return true;
+
+		else
 		return false;
 	}
 
@@ -29,7 +39,9 @@ public class Exercises {
 	 makePi() → [3, 1, 4]
 	 */
 	public int[] makePi() {
-		return new int[] {};
+
+		int[] pi = {3, 1, 4};
+		return pi;
 	}
 
 	/*
@@ -40,6 +52,11 @@ public class Exercises {
 	 commonEnd([1, 2, 3], [1, 3]) → true
 	 */
 	public boolean commonEnd(int[] a, int[] b) {
+
+		if((a[0] == b[0]) || (a[a.length-1] == b[b.length-1])){
+			return true;
+		}
+		else
 		return false;
 	}
 
@@ -50,7 +67,15 @@ public class Exercises {
 	 sum3([7, 0, 0]) → 7
 	 */
 	public int sum3(int[] nums) {
-		return 0;
+
+		int sum = 0;
+
+
+		for(int i = 0; i < nums.length; i++){
+			sum = sum + nums[i];
+		}
+
+		return sum;
 	}
 
 	/*
@@ -61,8 +86,11 @@ public class Exercises {
 	 rotateLeft3([7, 0, 0]) → [0, 0, 7]
 	 */
 	public int[] rotateLeft3(int[] nums) {
-		return new int[] {};
+
+	return new int[] {nums[1], nums[2], nums[0]};
 	}
+
+
 
 	/*
 	 7. Given an array of ints length 3, return a new array with the elements in reverse order, so 
@@ -72,7 +100,7 @@ public class Exercises {
 	 reverse3([7, 0, 0]) → [0, 0, 7]
 	 */
 	public int[] reverse3(int[] nums) {
-		return new int[] {};
+		return new int[] {nums[2], nums[1], nums[0]};
 	}
 
 	/*
@@ -83,7 +111,12 @@ public class Exercises {
 	 maxEnd3([2, 11, 3]) → [3, 3, 3]
 	 */
 	public int[] maxEnd3(int[] nums) {
-		return new int[] {};
+		if(nums[0] > nums[2]){
+
+		return new int[] {nums[0], nums[0], nums[0]}; }
+
+		else
+			return new int[] {nums[2], nums[2], nums[2]};
 	}
 
 	/*
@@ -94,7 +127,15 @@ public class Exercises {
 	 sum2([1, 1, 1, 1]) → 2
 	 */
 	public int sum2(int[] nums) {
-		return 0;
+
+		if(nums.length == 1){
+			return nums[0];}
+
+		if(nums.length == 0){
+			return 0;}
+
+		else
+			return nums[0] + nums[1];
 	}
 
 	/*
@@ -105,7 +146,7 @@ public class Exercises {
 	 middleWay([5, 2, 9], [1, 4, 5]) → [2, 4]
 	 */
 	public int[] middleWay(int[] a, int[] b) {
-		return new int[] {};
+		return new int[] {a[1], b[1]};
 	}
 
 	/*
@@ -116,7 +157,15 @@ public class Exercises {
 	 countEvens([1, 3, 5]) → 0
 	 */
 	public int countEvens(int[] nums) {
-		return 0;
+		int counter = 0;
+
+		for(int i=0; i<nums.length; i++){
+			if(nums[i] % 2 == 0)
+				counter++;
+
+
+		}
+		return counter;
 	}
 
 	/*
@@ -130,7 +179,14 @@ public class Exercises {
 	 sum13([1, 2, 2, 1, 13, 3, 4]) → 10
 	 */
 	public int sum13(int[] nums) {
-		return 0;
+		int sum=0;
+		for(int i=0; i<nums.length; i++){
+			if(nums[i]==13 || i > 0 && nums[i - 1] == 13) {
+				sum= sum - nums[i];
+			}
+			sum += nums[i];
+		}
+		return sum;
 	}
 
 	/*
@@ -140,6 +196,12 @@ public class Exercises {
 	 has22([2, 1, 2]) → false
 	 */
 	public boolean has22(int[] nums) {
+
+		for(int i=0; i<nums.length-1; i++){
+			if((nums[i]==2) && (nums[i+1]==2))
+				return true;}
+
+
 		return false;
 	}
 	
@@ -150,7 +212,12 @@ public class Exercises {
 	 lucky13([1, 2, 4]) → false
 	 */
 	public boolean lucky13(int[] nums) {
-		return false;
+		for(int i=0; i < nums.length; i++){
+			if((nums[i] == 1) || (nums[i] == 3)){
+				return false;
+			}
+		}
+		return true;
 	}
 
 	/*
@@ -160,7 +227,16 @@ public class Exercises {
 	 sum28([1, 2, 3, 4]) → false
 	 */
 	public boolean sum28(int[] nums) {
-		return false;
-	}
+		int sum = 0;
 
-}
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 2) {
+				sum = sum + 2;
+			}
+		}
+			if (sum == 8) {
+				return true;}
+
+			return false; }}
+
+
--- module-1/05_Command_Line_Programs/lecture-student/java/src/main/java/com/techelevator/DiscountCalculator.java
+++ module-1/05_Command_Line_Programs/lecture-student/java/src/main/java/com/techelevator/DiscountCalculator.java
@@ -1,5 +1,6 @@
 package com.techelevator;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 class DiscountCalculator {
@@ -9,18 +10,44 @@ class DiscountCalculator {
      */
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner myScanner = new Scanner(System.in);
 
         System.out.println("Welcome to the Discount Calculator");
 
         // Prompt the user for a discount amount
         // The answer needs to be saved as a double
-        System.out.print("Enter the discount amount (w/out percentage): ");
+        System.out.print("Enter the discount percent in whole numbers): ");
+        String userDiscPercentage = myScanner.nextLine();
+
+        System.out.println("You Entered: " + userDiscPercentage);
 
 
 
         // Prompt the user for a series of prices
         System.out.print("Please provide a series of prices (space separated): ");
+        String userPrices = myScanner.nextLine();
+
+        System.out.println("You Entered: " + userPrices);
+
+        // rely on wrapper classes to convert strings
+
+        double discount = Double.parseDouble(userDiscPercentage);
+
+        String[] strArrayPrices = userPrices.split(" ");
+                // Hungarian notation the first word is the type
+
+        for(int i = 0; i < strArrayPrices.length; i++){
+            //System.out.println(strArrayPrices[i]);
+
+            double originalPrice = Double.parseDouble(strArrayPrices[i]);
+
+            double discAmount = originalPrice * (discount/100.0);
+
+            double salePrice = originalPrice - discAmount;
+
+            System.out.println("Original Price: " + originalPrice + "Discount: " + discAmount + "Sale Price: " + salePrice);
+
+        }
 
 
 
