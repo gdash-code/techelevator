c073840ef195f8430b43eb40b5122049669e0e34
Rich Seeds <rich@techelevator.com>
2021-06-08 12:18:57 -0400

    Mod 2 Day 2 Exercise and Lecture Final

---
+++ module-2/02_Aggregate_Functions_and_GROUP_BY/exercise-student/postgres/Exercises/08_smallest_northeast_area.sql
@@ -0,0 +1,4 @@
+-- 8. The area of the state with the smallest population in the "Northeast" census region. Name the column 'smallest_northeast_area'.
+-- Expected answer is around 4,000
+-- (1 row)
+
